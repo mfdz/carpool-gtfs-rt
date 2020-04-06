f3d613d238886fcf00bec939a4fdfdca0cd225f0
Leonard Ehrenfried <mail@leonard.io>
2020-04-06 14:09:00 +0200

    Finish up feed building

--- src/main/java/de/mfdz/resource/CarpoolResource.java
+++ src/main/java/de/mfdz/resource/CarpoolResource.java
@@ -1,18 +1,18 @@
 package de.mfdz.resource;
 
+import static com.google.transit.realtime.GtfsRealtime.*;
+
 import com.codahale.metrics.annotation.Timed;
-import com.google.transit.realtime.GtfsRealtime;
+import com.google.transit.realtime.GtfsRealtimeConstants;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("carpool")
@@ -21,45 +21,51 @@ public class CarpoolResource {
 
     @GET
     @Timed
-    public byte[] sayHello(@QueryParam("name") Optional<String> name) {
+    public byte[] carpoolFeed() {
 
-        String today = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+        var today = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
 
-        GtfsRealtime.TripDescriptor tripDescriptor =
-                GtfsRealtime.TripDescriptor.newBuilder()
+        var tripDescriptor =
+                TripDescriptor.newBuilder()
                         .setTripId("carpool-update-123")
                         .setStartDate(today)
-                        .setScheduleRelationship(
-                                GtfsRealtime.TripDescriptor.ScheduleRelationship.ADDED)
+                        .setScheduleRelationship(TripDescriptor.ScheduleRelationship.ADDED)
                         .build();
 
-        String herrenbergId = "1:de:08115:4512:1:2";
-        String ehningenId = "1:de:08115:5773:1:1";
+        var herrenbergId = "1:de:08115:4512:1:2";
+        var ehningenId = "1:de:08115:5773:1:1";
 
-        ZonedDateTime threeOclock =
+        var threeOclock =
                 LocalDate.now().atStartOfDay().plusHours(15).atZone(ZoneId.of("Europe/Berlin"));
 
-        List<GtfsRealtime.TripUpdate.StopTimeUpdate> stopTimeUpdates =
+        var stopTimeUpdates =
                 Arrays.asList(
                         buildStopTimeUpdate(herrenbergId, threeOclock),
                         buildStopTimeUpdate(ehningenId, threeOclock.plusMinutes(30)));
 
-        GtfsRealtime.TripUpdate update =
-                GtfsRealtime.TripUpdate.newBuilder()
+        var update =
+                TripUpdate.newBuilder()
                         .setTrip(tripDescriptor)
                         .addAllStopTimeUpdate(stopTimeUpdates)
                         .build();
 
-        return update.toByteArray();
+        var header =
+                FeedHeader.newBuilder()
+                        .setIncrementality(FeedHeader.Incrementality.FULL_DATASET)
+                        .setTimestamp(Instant.now().getEpochSecond())
+                        .setGtfsRealtimeVersion(GtfsRealtimeConstants.VERSION);
+
+        var entity =
+                FeedEntity.newBuilder().setId(update.getTrip().getTripId()).setTripUpdate(update);
+
+        var feedMessage = FeedMessage.newBuilder().setHeader(header).addEntity(entity).build();
+
+        return feedMessage.toByteArray();
     }
 
-    private GtfsRealtime.TripUpdate.StopTimeUpdate buildStopTimeUpdate(
-            String stopId, ZonedDateTime time) {
-        GtfsRealtime.TripUpdate.StopTimeEvent timeEvent =
-                GtfsRealtime.TripUpdate.StopTimeEvent.newBuilder()
-                        .setTime(time.toEpochSecond())
-                        .build();
-        return GtfsRealtime.TripUpdate.StopTimeUpdate.newBuilder()
+    private TripUpdate.StopTimeUpdate buildStopTimeUpdate(String stopId, ZonedDateTime time) {
+        var timeEvent = TripUpdate.StopTimeEvent.newBuilder().setTime(time.toEpochSecond()).build();
+        return TripUpdate.StopTimeUpdate.newBuilder()
                 .setDeparture(timeEvent)
                 .setArrival(timeEvent)
                 .setStopId(stopId)
